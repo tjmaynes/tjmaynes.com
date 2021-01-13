768bf5373ed2c7de1c5fab3c5d569051c98e8fc1
TJ Maynes (Personal) <tj@tjmaynes.com>
2021-01-13 07:29:47 -0500

    Reduce number of lighthouse runs

--- test/performance/lighthouserc.js
+++ test/performance/lighthouserc.js
@@ -1,7 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: '../../public'
+      staticDistDir: '../../public',
+      numberOfRuns: 1
     },
     assert: {
       preset: 'lighthouse:no-pwa',
