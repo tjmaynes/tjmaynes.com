953df9cbb0a316bf9cd02a5385b6a8c92e6ebdd1
TJ Maynes <tj@tjmaynes.com>
2021-08-18 21:20:14 -0400

    Style blog posts

--- scripts/new-post.sh
+++ scripts/new-post.sh
@@ -31,8 +31,9 @@ function main() {
 ---
 layout: post
 title: "${POST_TITLE}"
+author: tjmaynes
 date: ${POST_DATE}
-background: /${NEW_POST_IMAGE_DIRECTORY}/background.jpg
+image: /${NEW_POST_IMAGE_DIRECTORY}/background.jpg
 published: true 
 ---
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
